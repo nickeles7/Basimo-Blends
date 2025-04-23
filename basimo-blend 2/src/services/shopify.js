ebb005b455cbdb2e38c74647eb608e84768e3b72
Nickeles Kerkinni <66744057+nickeles7@users.noreply.github.com>
2025-04-23 12:20:48 -0400

    Delete basimo-blend 2 directory

--- basimo-blend 2/src/services/shopify.js
+++
@@ -1,8 +0,0 @@
-import Client from 'shopify-buy';
-
-const client = Client.buildClient({
-  domain: process.env.REACT_APP_SHOPIFY_DOMAIN || '',
-  storefrontAccessToken: process.env.REACT_APP_SHOPIFY_STOREFRONT_ACCESS_TOKEN || '',
-});
-
-export default client;
